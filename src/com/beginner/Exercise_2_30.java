6e6b46657e80d8ae05befcae1cb31ee6875a616f
Oluwashetemipe <oluwashetemipe@gmail.com>
2021-12-13 17:53:56 -0800

    Updated

--- src/com/beginner/Exercise_2_30.java
+++ src/com/beginner/Exercise_2_30.java
@@ -8,11 +8,23 @@ public class Exercise_2_30 {
         System.out.println("Please enter a 5 numbers: ");
         int digits = input.nextInt();
         int firstNumber;
-        int secondNumber;
-        int thirdNumber;
-        int fourthNumber;
-        int fifthNumber;
-        firstNumber = digits / 10000;
-        secondNumber = (digits);
+        if (digits >= 10000 && digits < 100000){
+            int secondNumber;
+            int thirdNumber;
+            int fourthNumber;
+            int fifthNumber;
+            firstNumber = digits / 10000;
+            secondNumber = (digits % 10000) / 1000;
+            thirdNumber = (digits % 1000) / 100;
+            fourthNumber = (digits % 100) / 10;
+            fifthNumber = (digits % 10);
+            System.out.printf("%d   %d   %d   %d   %d",firstNumber , secondNumber,thirdNumber,fourthNumber,fifthNumber);
+        }
+        if (digits < 10000){
+            System.out.println("You didn't enter a 5 digit number");
+        }
+        if (digits >= 100000){
+            System.out.println("You didn't enter a 5 digit number");
+        }
     }
 }
